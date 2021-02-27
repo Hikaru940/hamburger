653a5cb3af33f15b317fa5327c6748f602d289a1
Hikaru Takasugi <gaoshanrijialiu@gmail.com>
2021-02-27 20:09:15 +0900

    add new file

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ $(function() {
     $('.c-menu').click(function() {
         $('.p-sidebar').addClass('js-open');
         $('body').addClass('js-open');
-        $('.l-header__left').addClass('js-open');
-        $('.c-search').addClass('js-open');
-        $('.c-submit').addClass('js-open');
+        $('.c-black-bg').addClass('js-black-bg');
     });
 
     $('.c-close').click(function() {
         $('.p-sidebar').removeClass('js-open');
         $('body').removeClass('js-open');
-        $('.l-header__left').removeClass('js-open');
-        $('.c-search').removeClass('js-open');
-        $('.c-submit').removeClass('js-open');
+        $('.c-black-bg').removeClass('js-black-bg');
     })
 });
